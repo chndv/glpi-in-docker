ff6d32bfd4b113b17248a3c432516cf4a480eb11
Stanislav Chindyaev <chndv@realweb.ru>
2025-01-29 11:48:59 +0300

    fix: Зависание по причине параметра в php-fpm
    Change pm.max_children param
    modified:   docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i 's/pm.max_children = 5/pm.max_children = 10/g' /usr/local/etc/php-fpm.d/www.conf
 echo "date.timezone = \"${TZ}\"" > /usr/local/etc/php/conf.d/timezone.ini
 
 crond -b -l 2
